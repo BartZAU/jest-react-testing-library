f955e0450b6c7d512a56948ae6703785c4b74853
BartZabielski <bart.zabielski@protonmail.com>
2021-10-30 21:50:31 +1100

    first

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 https://www.udemy.com/course/react-testing-library/learn/lecture/24418712#overview
 
+
+this codebase repo
+https://github.com/DigitialRambo/jest-react-testing-library
+
+other testing ive done
+https://github.com/DigitialRambo/jest-react-testing
+
--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,22 +1,22 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('button has correct initial color', () => {
-	render(<App />);
+test("button has correct initial color", () => {
+  render(<App />);
 
-	// find an element with a role of button and text of 'Change to blue'
-	const colorButton = screen.getByRole('button', {name: /change to blue/i});
+  // find an element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole("button", { name: /change to blue/i });
 
-	// expect the background color to be red
-	expect(colorButton).toHaveStyle({backgroundColor: 'red'})
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
 
-	// click button
-	fireEvent.click(colorButton);
+  // click button
+  fireEvent.click(colorButton);
 
-	// expect the background color to be blue
-	expect(colorButton).toHaveStyle({backgroundColor: 'blue'});
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
 
-	// expect the buttont ext to be 'Change to red
-	expect(colorButton.textContent).toBe(/change to red/i)
+  // expect the buttont ext to be 'Change to red
+  // toBe does not accept regular expression
+  expect(colorButton.textContent).toBe("Change to red");
 });
-
